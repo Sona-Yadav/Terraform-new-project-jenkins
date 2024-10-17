34f5c0ae16c5e46b629a54e330fb7c5a9d893c75
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2024-10-17 22:12:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region1" {
+  default = "us-east-1"
+}
+
+variable "region2" {
+  default = "us-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
